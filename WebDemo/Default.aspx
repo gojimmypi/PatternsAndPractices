e87f61c5c9ad449333b0d264d946ae8d480e7de2
gojimmypi <gojimmypi@gmail.com>
2017-12-14 15:43:55 -0800

    initial code for C# ASP.Net web application to demo SQLHelper

---
+++ WebDemo/Default.aspx
@@ -0,0 +1,30 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebDemo._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <table border="1" id="tblDatabases"  style="width: auto">
+        <asp:Repeater ID="DatabaseList" runat="server" Visible="True">
+            <HeaderTemplate>
+                <thead>
+                    <tr>
+                        <th>
+                            Database Name
+                        </th>
+                    </tr>
+                </thead>
+            </HeaderTemplate>
+            <ItemTemplate>
+                <tr>
+                    <td>
+                        <span title='<%# DataBinder.Eval(Container.DataItem, "state_desc") %>'>
+                            <%# DataBinder.Eval(Container.DataItem, "name") %>
+                        </span>
+                    </td>
+                </tr>
+            </ItemTemplate>
+        </asp:Repeater>
+    </table>
+
+
+
+</asp:Content>
---
+++ WebDemo/Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using Microsoft.ApplicationBlocks.Data;
+namespace WebDemo
+{
+    public partial class _Default : Page
+    {
+        const string MY_SERVER = "hadev12";
+        const string MY_DATABASE = "master";
+
+        //***********************************************************************************************************************************
+        static string ConnectionString(string strServerName, string strDatabaseName)
+        //***********************************************************************************************************************************
+        {
+            // see http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpref/html/frlrfSystemDataSqlClientSqlConnectionClassConnectionStringTopic.asp
+            //
+            // there is some debate as to whether the Oledb provider is indeed faster than the native client!
+            //  
+            return "Workstation ID=myDemo;" +
+                   "packet size=8192;" +
+                   "Persist Security Info=false;" +
+                   "Server=" + strServerName + ";" +
+                   "Database=" + strDatabaseName + ";" +
+                   "Trusted_Connection=true; " +
+                   "Network Library=dbmssocn;" +
+                   "Pooling=True; " +
+                   "Enlist=True; " +
+                   "Connection Lifetime=14400; " +
+                   "Max Pool Size=20; Min Pool Size=0";
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string strSQL = "select * from sys.databases";
+            this.DatabaseList.DataSource = SqlHelper.ExecuteDataset(ConnectionString(MY_SERVER, MY_DATABASE), CommandType.Text, strSQL );
+            this.DatabaseList.DataBind();
+        }
+    }
+}
